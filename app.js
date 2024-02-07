34f54825b1533c0ca9dd59b68696efaa1111e454
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-02-07 00:33:04 -0400

    Add routes to get requests for filter

--- app.js
+++ app.js
@@ -7,7 +7,24 @@ app.disable('x-powered-by')
 const movies = readJSON('./movies.json')
 
 app.get('/movies', (req, res) => {
-  res.json(movies)
+  const { genre } = req.query
+	if (genre) {
+		const filteredMovies = movies.filter(
+			movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+		)
+		
+		return res.json(filteredMovies)
+	}
+	
+	return res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+	const { id } = req.params
+	const movie = movies.find(movie => movie.id === id)
+	if (movie) return res.json(movie)
+	
+	res.status(404).json({ message: 'Movie not found' })
 })
 
 const PORT = process.env.PORT ?? 1234
