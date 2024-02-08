07c74685a31da67377cbf13ced4beb445b401eac
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-02-08 10:48:34 -0400

    Create controllers and implement its used

---
+++ controllers/movies.js
@@ -0,0 +1,46 @@
+import { validateMovie, validatePartialMovie } from './schemas/movies.js'
+import { MovieModel } from '../models/movies.js'
+
+export class MovieController {
+  static async getAll(req, res) {
+    const { genre } = req.query
+    const movies = await MovieModel.getAll({ genre })
+
+    res.json(movies)
+  }
+
+  static async getById(req, res) {
+    const { id } = req.params
+    const movie = await MovieModel.getById(id)
+
+    if (movie) return res.json(movie)
+
+    res.status(404).json({ message: 'Movie not found' })
+  }
+
+  static async create(req, res) {
+    const result = validateMovie(req.body)
+
+    if (result.error) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const newMovie = await MovieModel.create(result.data)
+    res.status(201).json(newMovie)
+  }
+
+  static async update(req, res) {
+    const result = validatePartialMovie(req.body)
+
+    if (result.error) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const { id } = req.params
+    const updateMovie = await MovieModel.update(id, result.data)
+
+    if (updateMovie) return res.json(updateMovie)
+
+    res.status(404).json({ message: 'Movie not found' })
+  }
+}
--- routes/movies.js
+++ routes/movies.js
@@ -1,43 +1,9 @@
 import { Router } from 'express'
-import { validateMovie, validatePartialMovie } from './schemas/movies.js'
-import { MovieModel } from '../models/movies.js'
+import { MovieController } from '../controllers/movies.js'
 
 export const moviesRouter = Router()
 
-app.get('/', async(req, res) => {
-  const { genre } = req.query
-	const movies = await MovieModel.getAll({ genre })
-	res.json(movies)
-})
-
-app.get('/:id', async (req, res) => {
-	const { id } = req.params
-	const movie = await MovieModel.getById(id)
-	if (movie) return res.json(movie)
-	
-	res.status(404).json({ message: 'Movie not found' })
-})
-
-app.post('/', async (req, res) => {
-	const result = validateMovie(req.body)
-	
-	if (result.error) {
-		return res.status(400).json({ error: JSON.parse(result.error.message) })
-	}
-	
-	const newMovie = await MovieModel.create(result.data)
-	res.status(201).json(newMovie)
-})
-
-app.patch('/:id', async (req, res) => {
-	const result = validatePartialMovie(req.body)
-	
-	if (result.error) {
-		return res.status(400).json({ error: JSON.parse(result.error.message) })
-	}
-	
-	const { id } = req.params
-	const updateMovie = await MovieModel.update(id, result.data)
-	
-	return res.json(updateMovie)
-})
+app.get('/', MovieController.getAll)
+app.get('/:id', MovieController.getById)
+app.post('/', MovieController.create)
+app.put('/:id', MovieController.update)
