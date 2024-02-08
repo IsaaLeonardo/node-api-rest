de153e499033bd2d339d4e3ba554209590af6ce3
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-02-08 09:41:30 -0400

    Use cors library

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 import express from 'express'
+import cors from 'cors'
 import { readJSON } from './utils.js'
 import { validateMovie, validatePartialMovie } from './schemas/movies.js'
 
 const app = express()
 app.disable('x-powered-by')
 app.use(express.json());
+
+app.use(cors({
+		origin: (origin, callback) => {
+			const ACCEPTED_ORIGINS = [
+				'http://localhost:8080',
+				'http://localhost:1234',
+			]
+			
+			if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+				return callback(null, true)
+			}
+			
+			return callback(new Error('Not Allowed by CORS'))
+		}
+	})
+)
 
 const movies = readJSON('./movies.json')
 
